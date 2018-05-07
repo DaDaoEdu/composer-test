3e8f4bcdc928f261a25bb683a235c3f0a759d8a3
Rober Gao <1379596618@qq.com>
2018-05-07 17:19:36 +0800

    this is test

--- src/DaDaoEdu/Car/Benz.php
+++ src/DaDaoEdu/Car/Benz.php
@@ -6,7 +6,7 @@ class Benz
 {
     public static function info()
     {
-        echo "This is Benz Car.<br />";
+        echo "This is Benz Car.1111<br />";
     }
 
     public static function product()
